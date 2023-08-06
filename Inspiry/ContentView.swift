3148097fd90ac96181c3dbfd7837958d6731a1e1
Alex <56194510+rudberga@users.noreply.github.com>
2023-08-06 23:24:08 +0900

    Initial Commit

---
+++ Inspiry/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Inspiry
+//
+//  Created by Alexander Rudberg on 2023-08-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Inspiry/InspiryApp.swift
@@ -0,0 +1,17 @@
+//
+//  InspiryApp.swift
+//  Inspiry
+//
+//  Created by Alexander Rudberg on 2023-08-06.
+//
+
+import SwiftUI
+
+@main
+struct InspiryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
